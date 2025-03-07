0cb7f98ccd5ab59985d815038833444f25476e0b
João Pedro Quadro Carvalho <jpgnoatto15@gmail.com>
2025-03-07 08:17:47 -0300

    Update Mesa.java

--- Mesa.java
+++ Mesa.java
@@ -5,11 +5,16 @@ public class Mesa {
 
     // Construtores
     public Mesa() {} // Construtor padrão
+
     public Mesa(int numero, int capacidade) { // Construtor parametrizado
+        if (numero < 1 || numero > 10) {
+            throw new IllegalArgumentException("Número da mesa deve estar entre 1 e 10.");
+        }
         this.numero = numero;
         this.capacidade = capacidade;
         this.ocupada = false;
     }
+
     public Mesa(Mesa outraMesa) { // Construtor de cópia
         this.numero = outraMesa.numero;
         this.capacidade = outraMesa.capacidade;
@@ -20,18 +25,26 @@ public class Mesa {
     public int getNumero() {
         return numero;
     }
+
     public void setNumero(int numero) {
+        if (numero < 1 || numero > 10) {
+            throw new IllegalArgumentException("Número da mesa deve estar entre 1 e 10.");
+        }
         this.numero = numero;
     }
+
     public int getCapacidade() {
         return capacidade;
     }
+
     public void setCapacidade(int capacidade) {
         this.capacidade = capacidade;
     }
+
     public boolean isOcupada() {
         return ocupada;
     }
+
     public void setOcupada(boolean ocupada) {
         this.ocupada = ocupada;
     }
