e013a41a1000e65829489ca1142032b924a02f73
João Pedro Quadro Carvalho <jpgnoatto15@gmail.com>
2025-04-09 19:50:39 -0300

    Update ClienteVIP.java

--- ClienteVIP.java
+++ ClienteVIP.java
@@ -1,25 +1,46 @@
+/**
+ * Classe que representa um cliente VIP, herda de Cliente.
+ * Possui um atributo adicional de desconto.
+ */
 public class ClienteVIP extends Cliente {
     private double desconto;
 
-    // Construtor
+    /**
+     * Construtor para ClienteVIP.
+     * @param nome Nome do cliente
+     * @param telefone Telefone do cliente
+     * @param desconto Percentual de desconto (0-100)
+     * @throws IllegalArgumentException Se o desconto for inválido
+     */
     public ClienteVIP(String nome, String telefone, double desconto) {
-        super(nome, telefone); // Chama o construtor da superclasse
-        this.desconto = desconto;
+        super(nome, telefone);
+        this.setDesconto(desconto);
     }
 
     // Getter e Setter específico
+    
     public double getDesconto() {
         return desconto;
     }
 
+    /**
+     * Define o desconto do cliente VIP.
+     * @param desconto Percentual de desconto (0-100)
+     * @throws IllegalArgumentException Se o desconto for inválido
+     */
     public void setDesconto(double desconto) {
+        if (desconto < 0 || desconto > 100) {
+            throw new IllegalArgumentException("Desconto deve estar entre 0 e 100%.");
+        }
         this.desconto = desconto;
     }
 
-    // Sobrescreve o método da superclasse
+    /**
+     * Sobrescreve o método da superclasse para incluir informações do VIP.
+     */
     @Override
     public void exibirDetalhes() {
-        super.exibirDetalhes(); // Chama o método da superclasse
+        super.exibirDetalhes();
         System.out.println("Desconto VIP: " + desconto + "%");
     }
 }
